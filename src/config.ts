9465a11ab9753bbfcb2cc620838e9054f77f4ec0
kryota-dev <50436249+kryota-dev@users.noreply.github.com>
2025-06-02 20:48:32 +0900

    ✨ 環境変数NEXT_PUBLIC_HOME_URLを追加し、レイアウトにサイトのメタデータを統合しました。また、サイト名と説明を定義する定数ファイルを新規作成し、OGP画像を追加しました。

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 // 環境変数のBarrelファイル
 
 export const NEXT_PUBLIC_GTM_ID = process.env.NEXT_PUBLIC_GTM_ID
+export const NEXT_PUBLIC_HOME_URL = process.env.NEXT_PUBLIC_HOME_URL
---
+++ src/constants/SITE_DESCRIPTION.ts
@@ -0,0 +1,5 @@
+/**
+ * サイトの説明
+ * @description TODO: サイト公開時に変更する
+ */
+export const SITE_DESCRIPTION = 'This is a template for Next.js static export.'
