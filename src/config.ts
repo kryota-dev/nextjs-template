015466fa8d4dc4730a1c538f2c61c1d1d4bbd05a
kryota-dev <50436249+kryota-dev@users.noreply.github.com>
2025-06-02 19:27:25 +0900

    ✨ Google Tag Managerの設定を更新し、GTM_IDをNEXT_PUBLIC_GTM_IDに変更しました。また、GoogleTagManagerコンポーネントのテストを新規作成し、GTM_IDが設定されている場合の動作を確認するテストケースを追加しました。

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // 環境変数のBarrelファイル
 
-export const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID
+export const NEXT_PUBLIC_GTM_ID = process.env.NEXT_PUBLIC_GTM_ID
--- src/libs/GoogleTagManager/GoogleTagManager.tsx
+++ src/libs/GoogleTagManager/GoogleTagManager.tsx
@@ -1,30 +1,30 @@
 import { GoogleTagManager as NextGTM } from '@next/third-parties/google'
 
-import { GTM_ID } from '@/config'
+import { NEXT_PUBLIC_GTM_ID } from '@/config'
 
 /**
  * Google Tag Managerのscriptタグを生成するコンポーネント
- * @description `GTM_ID`が設定されていない場合はnullを返す
+ * @description `NEXT_PUBLIC_GTM_ID`が設定されていない場合はnullを返す
  */
 export const GoogleTagManager = () => {
-  if (!GTM_ID) return null
+  if (!NEXT_PUBLIC_GTM_ID) return null
 
-  return <NextGTM gtmId={GTM_ID} />
+  return <NextGTM gtmId={NEXT_PUBLIC_GTM_ID} />
 }
 
 /**
  * Google Tag Managerのnoscriptタグを生成するコンポーネント
- * @description `GTM_ID`が設定されていない場合はnullを返す
+ * @description `NEXT_PUBLIC_GTM_ID`が設定されていない場合はnullを返す
  */
 export const GoogleTagManagerNoscript = () => {
-  if (!GTM_ID) return null
+  if (!NEXT_PUBLIC_GTM_ID) return null
 
   return (
-    <noscript>
+    <noscript data-testid='gtm-noscript' data-test-gtm-id={NEXT_PUBLIC_GTM_ID}>
       <iframe
         title='Google Tag Manager'
         loading='lazy'
-        src={`//www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+        src={`//www.googletagmanager.com/ns.html?id=${NEXT_PUBLIC_GTM_ID}`}
         height='0'
         width='0'
         style={{ display: 'none', visibility: 'hidden' }}
