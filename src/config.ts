b27baaaacab3d761bc2249eb70fad528d80f23cf
kryota-dev <50436249+kryota-dev@users.noreply.github.com>
2025-06-02 18:45:38 +0900

    ✨ Google Tag Managerの設定を追加し、環境変数からGTM_IDを取得するようにしました。また、レイアウトにGTMを組み込み、noscriptタグを追加しました。

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,3 @@
-/**
- * 環境変数のBarrelファイル
- *
- * @example 環境変数を定義する場合
- * ```ts
- * export const GA_ID = process.env.GA_ID
- * ```
- *
- * @example 環境変数を参照する場合
- * ```ts
- * import { GA_ID as GA_ID_ENV } from '@/config'
- *
- * if (!GA_ID_ENV) {
- *   throw new Error('GA_ID is required')
- * }
- * const GA_ID = GA_ID_ENV
- * ```
- */
+// 環境変数のBarrelファイル
+
+export const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID
